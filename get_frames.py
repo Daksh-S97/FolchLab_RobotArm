8506099c7d686138787c6b4076e2dda85b76defa
steiva <ivan.d.stepanov@gmail.com>
2022-06-15 13:46:10 -0700

    Camera calibration deps

---
+++ get_frames.py
@@ -0,0 +1,28 @@
+import numpy as np
+import cv2
+
+
+
+# Create videoCapture objects for both video streams.
+cap = cv2.VideoCapture(0)
+
+num = 0
+
+# Set infinite loop to capture images from video.
+while True:
+    # We use the .grab() method to reduce the lag between the two videos.
+    ret, frame = cap.read()
+
+    k = cv2.waitKey(5)
+    if k == 27:
+        break
+    elif k == ord('s'):  # wait for 's' key to save and exit
+        # Put whatever directory is convenient for you.
+        cv2.imwrite('data/image' +
+                    str(num) + '.png', frame)
+        print("images saved!")
+        num += 1
+    # Displaying the capture in a single window, more convenient.
+    cv2.imshow('Concat', frame)
+
+cv2.destroyAllWindows()
